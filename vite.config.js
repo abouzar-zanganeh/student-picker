07ffee10a611283a3278b4c6c537435d03e15c8f
abouzar-zanganeh <abouzar.zanganeh@gmail.com>
2025-08-22 15:55:27 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,30 +2,29 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+    base: '/student-picker/',
     plugins: [
         VitePWA({
-			base: '/student-picker/',
             registerType: 'autoUpdate',
             manifest: {
                 name: 'دستیار هوشمند معلم',
                 short_name: 'دستیار معلم',
-                description: 'یک ابزار تحت وب مدرن و هوشمند برای کمک به معلمان در مدیریت کلاس، ثبت دقیق فعالیت‌ها و انتخاب عادلانه دانش‌آموزان.',
+                description: 'یک ابزار تحت وب مدرن و هوشمند برای کمک به معلمان در مدیریت کلاس، ثبت دقیق فعالیت‌ها و انتخاب عادلانه دانش‌آ-آموزان.',
                 theme_color: '#007bff',
                 icons: [
                     {
-                        src: 'images/icon-192.png', // The path is relative to the public folder
+                        src: 'images/icon-192.png',
                         sizes: '192x192',
                         type: 'image/png'
                     },
                     {
-                        src: 'images/icon-512.png', // Make sure you have a 512x512 icon in public/images for full PWA support
+                        src: 'images/icon-512.png',
                         sizes: '512x512',
                         type: 'image/png'
                     }
                 ]
             },
             workbox: {
-                // This tells the service worker to cache all generated assets, including your web fonts.
                 globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}']
             }
         })
